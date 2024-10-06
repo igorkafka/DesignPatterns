01d054237e7a60d431e97c8c5930b718c5869182
Igor-Mateus <Igor.Mateus@e-hps.com>
2024-10-05 21:00:43 -0300

    Added new patterns

---
+++ LSP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;    
+        }
+        public Rectangle()
+        {
+            
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width} {nameof(Height)}: {Height}";
+        }
+    }
+    public class Square : Rectangle 
+    {
+        public override int Width 
+        { 
+            set { base.Width = base.Height = value; } 
+        }
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+    internal class Program
+    {
+        static public int Area(Rectangle rectangle) => rectangle.Width * rectangle.Height;  
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(2,3);
+            Console.WriteLine(rectangle);
+
+            Rectangle square = new Square();
+            square.Width = 4;
+            square.Height = 9;
+            Console.WriteLine(square);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solid/ISP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public class Document
+    {
+
+
+    }
+    public interface IMachine
+    {
+        void Print(Document document);
+        void Scan(Document document);
+        void Fax(Document document);    
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document document)
+        {
+
+        }
+
+        public void Print(Document document)
+        {
+        }
+
+        public void Scan(Document document)
+        {
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
